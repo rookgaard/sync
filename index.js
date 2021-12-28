ff787116dd55596b0d645f25e0f9a731290ac9dc
Michal Korejwo <korejwo.michal@gmail.com>
2021-12-28 12:19:56 +0100

    socket test

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ const mysqlEventWatcher = MySQLEvents({
 	password: config.dbPassword ? config.dbPassword : ''
 });
 const tables = JSON.parse(config.dbTables ? config.dbTables : '[]');
+const port = config.port ? config.port : 3000;
+const master = process.argv[2] === 'server';
+
+if (master) {
+	var app = require('http').createServer();
+	const socketServer = require('socket.io')(app);
+
+	app.listen(port, function () {
+		console.log('nasluchuje na porcie', app.address().port);
+	});
+
+	socketServer.on('connection', function (socket) {
+		console.log('socketServer', 'connection');
+
+		socket.on('test', function (data) {
+			console.log('test', data);
+		});
+	});
+} else {
+	const socketClient = require('socket.io-client')('http://localhost:' + port);
+	console.log(socketClient);
+
+	socketClient.on('connect', function () {
+		console.log('socketClient', 'connect');
+		socketClient.emit('test', Date.now());
+	});
+
+	socketClient.on('event', function (data) {
+		console.log('socketClient', 'event', data);
+	});
+
+	socketClient.on('disconnect', function () {
+		console.log('socketClient', 'disconnect');
+	});
+}
 
 function valuesChanged(oldRow, newRow, event) {
 	console.log(oldRow, newRow, event);
@@ -29,5 +64,5 @@ function valuesChanged(oldRow, newRow, event) {
 }
 
 for (const table of tables) {
-	mysqlEventWatcher.add(table, valuesChanged);
+//	mysqlEventWatcher.add(table, valuesChanged);
 }
